fc7093502641fa1936fb20c93ba9c89c88b8cb81
Ritika Rao <ritika312@hotmail.com>
2021-02-19 04:56:28 -0500

    Reading/Writing data to the database, and creating backend API routes and schemas

--- backend/app.js
+++ backend/app.js
@@ -3,30 +3,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv/config');
-const api = process.env.API_URL;
+
+app.use(cors());
+app.options('*', cors())
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 
-app.get(`${api}/products`, (req, res) =>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url',
-    }
-    res.send(product);
-})
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-app.post(`${api}/products`, (req, res) =>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-})
+const api = process.env.API_URL;
 
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -39,6 +40,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err);
 })
 
+//Server
 app.listen(3000, ()=>{
 
     console.log('server is running http://localhost:3000');
