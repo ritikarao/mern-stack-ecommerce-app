69e438c0cfba65d6647f805adae903d1268e65bf
Ritika Rao <ritika312@hotmail.com>
2021-02-19 02:42:27 -0500

    Set up the backend server

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+// http://localhost:3000/api/v1/products
+app.get('/', (req, res) =>{
+    res.send('hello API !');
+})
+
+app.listen(3000, ()=>{
+    console.log(api);
+    console.log('server is running http://localhost:3000');
+})
